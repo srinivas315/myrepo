1fc2bb88e853f6a29d96d488852d10b80cad34d2
srinivas315 <reddy.srinivas315@gmail.com>
2020-05-24 14:39:23 +0530

    test

---
+++ HelloWorld/src/main/java/com/srinivas/helloworld/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.srinivas.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@RequestMapping("/")
+	public String index() {
+		return "Hello Manisha! Thanks for learning Devops!";
+	}
+}
