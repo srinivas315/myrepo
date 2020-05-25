da5791f4a23e43b045b87639d73cfda64ef5afce
Srinivasreddy <reddy.srinivas315@gmail.com>
2020-05-25 08:18:44 +0530

    added

--- HelloWorld/src/main/java/com/srinivas/helloworld/HelloWorldController.java
+++ HelloWorld/src/main/java/com/srinivas/helloworld/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@RequestMapping("/")
 	public String index() {
-		return "Hello Manisha! Thanks for learning Devops!";
+		return "Hello Manisha! I miss you!";
 	}
 }
